7ea79fad64d989f7cf293e81229495dc66ab3f25
David Rodriguez <davidrodriguez1470@gmail.com>
2021-11-08 11:27:26 +0100

    Clean web

---
+++ js/dscript.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("HOLA");
+    var addCart = document.getElementById("addCart");
+    addCart.addEventListener("click",addToCart)
+});
+
+function addToCart(e){
+    if(e.target.getElementById("addCart")){
+        productName = 
+    }
+    
+}
