20c86beb66ecbc1095bb4fa8edf601fe9eae77a5
David Rodriguez <davidrodriguez1470@gmail.com>
2021-11-09 13:46:35 +0100

    Update AddToCart Function

--- js/dscript.js
+++ js/dscript.js
@@ -1,3 +1,5 @@
+var carts = [];
+
 document.addEventListener("DOMContentLoaded", function () {
     console.log("HOLA");
     let productos = document.getElementById("productos");
@@ -10,10 +12,34 @@ function addToCart(e) {
 
     if (e.target.classList.contains("addCart")) {
         let producto = e.target;
-        console.log("AÑADIDO el producto con ID: " + producto.id);
-        console.log("NOMBRE: " + producto.parentNode.children[1].innerText);
-        console.log("PRECIO: " + producto.parentNode.children[2].innerText);
+
+        let id = producto.id;
+        let name = producto.parentNode.children[1].innerText;
+        let price = producto.parentNode.children[2].innerText;
+
+        console.log("AÑADIDO el producto con ID: " + id);
+        console.log("NOMBRE: " + name);
+        console.log("PRECIO: " + price);
+        
+        let productoIgual = carts.find(element => element.id == id);
+        let index = carts.indexOf(productoIgual);
+
+        if(productoIgual != null){
+            console.log("YA EXISTE!");
+            carts[index].quantity++;
+            console.log(carts[index].quantity);
+        } else {
+            carts.push({
+                id: id,
+                name: name,
+                price: price,
+                quantity: 1
+            });
+        }
 
     }
 
+    
+
 }
+
